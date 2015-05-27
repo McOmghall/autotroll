ae267c3cdaa9b470c539c0b9c0251378b6f7fe00
pmontoto <pmgberm@gmail.com>
2015-05-27 12:46:43 +0200

    Corrected script to get access tokens.

--- get_access_tokens.rb
+++ get_access_tokens.rb
@@ -6,28 +6,33 @@ logger = Logger.new(STDOUT)
 # Hax to bypass SSL verification
 OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
-options_twitter = {:consumer_key => ARGV[0], :consumer_secret => ARGV[1]}
+options_twitter = {:consumer_key => ARGV[0].chomp, :consumer_secret => ARGV[1].chomp}
 
-logger.info "Logging status #{options_twitter.to_s}"
+logger.info "Logging status: #{options_twitter.inspect}"
 
 client = TwitterOAuth::Client.new options_twitter
+
+logger.info "Client: #{client.inspect}"
 
 request_token = client.authentication_request_token(
   :oauth_callback => 'oob'
 )
 
+logger.info "Request token: #{request_token.inspect}"
+logger.info "Request token: #{request_token.token}"
+logger.info "Request token: #{request_token.secret}"
 
-if (ARGV[3] == nil)
-  logger.info "Go to #{request_token.authorize_url}"
-else
-  access_token = client.authorize(
-    request_token.token,
-    request_token.secret,
-    :oauth_verifier => ARGV[3]
-  )
+logger.info "Go to #{request_token.authorize_url}&force_login=true"
+puts "Input your pincode: "
+
+access_token = client.authorize(
+  request_token.token,
+  request_token.secret,
+  :oauth_verifier => $stdin.gets.chomp
+)
   
-  options_twitter[:access_token]        = access_token.token
-  options_twitter[:access_token_secret] = access_token.secret
+options_twitter[:access_token]        = access_token.token
+options_twitter[:access_token_secret] = access_token.secret
   
-  logger.info "Logging status #{options_twitter.to_s}"
-end
+logger.info "Result: #{options_twitter.inspect}"
+
